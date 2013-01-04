ef9228a245fa6710808268a61644e278ad4197ad
Caoyuan Deng <dcaoyuan@gmail.com>
2013-01-05 00:15:45 +0800

    Rewrite java sources to scala.

---
+++ scala.console/src/main/scala/org/netbeans/modules/scala/console/ScalaConsoleAction.scala
@@ -0,0 +1,22 @@
+package org.netbeans.modules.scala.console
+
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
+import javax.swing.ImageIcon
+import org.openide.util.ImageUtilities
+import org.openide.util.NbBundle
+
+/**
+ * Action which shows Scala Console component.
+ */
+class ScalaConsoleAction extends AbstractAction(NbBundle.getMessage(classOf[ScalaConsoleAction], "CTL_ScalaConsoleAction")) {
+    
+  putValue(javax.swing.Action.SMALL_ICON, new ImageIcon(ImageUtilities.loadImage(ScalaConsoleTopComponent.ICON_PATH, true)))
+    
+  def actionPerformed(evt: ActionEvent) {
+    val win = ScalaConsoleTopComponent.findInstance
+    win.open
+    win.requestActive
+  }
+    
+}
