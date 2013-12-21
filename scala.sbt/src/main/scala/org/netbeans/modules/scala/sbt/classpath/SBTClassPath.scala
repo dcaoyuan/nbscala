95137ffc9806a9d58aec9f7026ab993d391aa110
Caoyuan Deng <dcaoyuan@gmail.com>
2013-12-21 21:49:28 +0800

    Added classpath of dependencies project recursively.

--- scala.sbt/src/main/scala/org/netbeans/modules/scala/sbt/classpath/SBTClassPath.scala
+++ scala.sbt/src/main/scala/org/netbeans/modules/scala/sbt/classpath/SBTClassPath.scala
@@ -8,6 +8,8 @@ import org.netbeans.api.java.classpath.ClassPath
 import org.netbeans.api.java.platform.JavaPlatformManager
 import org.netbeans.api.project.FileOwnerQuery
 import org.netbeans.api.project.Project
+import org.netbeans.api.project.ProjectManager
+import org.netbeans.modules.scala.sbt.project.SBTProject
 import org.netbeans.modules.scala.sbt.project.SBTResolver
 import org.netbeans.spi.java.classpath.ClassPathImplementation
 import org.netbeans.spi.java.classpath.PathResourceImplementation
@@ -15,6 +17,7 @@ import org.netbeans.spi.java.classpath.support.ClassPathSupport
 import org.openide.filesystems.FileStateInvalidException
 import org.openide.filesystems.FileUtil
 import org.openide.util.Exceptions
+import scala.annotation.tailrec
 
 /**
  *
@@ -45,31 +48,57 @@ final class SBTClassPath(project: Project, scope: String, isTest: Boolean) exten
       result.addAll(getJavaBootResources)
     }
 
-    for (file <- sbtResolver.getResolvedClassPath(scope, isTest)) {
-      val fo = FileUtil.toFileObject(file)
+    getResolvedClassPath(sbtResolver, isTest, result)
+    getDepProjectsClasspath(sbtResolver, isTest, result)
+
+    result
+  }
+
+  private def getResolvedClassPath(resolver: SBTResolver, isTest: Boolean, result: java.util.ArrayList[PathResourceImplementation]) {
+    for {
+      file <- resolver.getResolvedClassPath(scope, isTest)
+      fo = FileUtil.toFileObject(file)
+    } {
       try {
-        val rootUrl =
-          if (fo != null) {
-            if (FileUtil.isArchiveFile(fo)) {
-              FileOwnerQuery.markExternalOwner(fo, project, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT)
-              FileUtil.getArchiveRoot(fo).toURL
-            } else {
-              file.toURI.toURL
-            }
-          } else { // fo is null, file does not exist
-            // file is a classes/srcs *folder* and may not exist yet, we must add a slash at the end.
-            // to tell ClassPathSupport that it's a folder instead of a general file
-            // @Note should avoid url string ends with doubled "/", i.e. "//"
-            val uriStr = file.toURI.toString
-            URI.create(if (uriStr.endsWith("/")) uriStr else uriStr + "/").toURL
+        val rootUrl = if (fo != null) {
+          if (FileUtil.isArchiveFile(fo)) {
+            FileOwnerQuery.markExternalOwner(fo, project, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT)
+            FileUtil.getArchiveRoot(fo).toURL
+          } else {
+            file.toURI.toURL
           }
+        } else { // fo is null, file does not exist
+          // file is a classes/srcs *folder* and may not exist yet, we must add a slash at the end.
+          // to tell ClassPathSupport that it's a folder instead of a general file
+          // @Note should avoid url string ends with doubled "/", i.e. "//"
+          val uriStr = file.toURI.toString
+          URI.create(if (uriStr.endsWith("/")) uriStr else uriStr + "/").toURL
+        }
         result.add(ClassPathSupport.createResource(rootUrl))
       } catch {
         case ex: FileStateInvalidException => Exceptions.printStackTrace(ex)
       }
     }
+  }
 
-    result
+  private def getDepProjectsClasspath(resolver: SBTResolver, isTest: Boolean, result: java.util.ArrayList[PathResourceImplementation]) {
+    for {
+      file <- resolver.getDependenciesProjects(isTest)
+      fo = FileUtil.toFileObject(file) if fo != null && fo.isFolder
+    } {
+      ProjectManager.getDefault.findProject(fo) match {
+        case project: SBTProject =>
+          val subResolver = project.getLookup.lookup(classOf[SBTResolver])
+          if (subResolver != null) {
+            getResolvedClassPath(subResolver, isTest, result)
+            val subDepProjects = subResolver.getDependenciesProjects(isTest)
+            if (subDepProjects.length > 0) {
+              getDepProjectsClasspath(subResolver, isTest, result)
+            }
+          }
+        case _ =>
+      }
+    }
   }
 
   private def getJavaBootResources: java.util.List[PathResourceImplementation] = {
